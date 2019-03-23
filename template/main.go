ac0d37bd92dc34567f9071631d260c11264d7e15
cook-a-doodle-do <densukenul@gmail.com>
2019-03-24 03:49:43 +0900

    update test of main function

--- template/main.go
+++ template/main.go
@@ -2,35 +2,50 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	basicIO "io"
 	"os"
 	"strconv"
 )
 
 func main() {
-	input := NewInPut()
-	for i := 0; i < 3; i++ {
-		println(input.nextString())
-	}
-
+	protagonist(os.Stdin, os.Stdout)
 }
 
-type InPut struct {
+func protagonist(r basicIO.Reader, w basicIO.Writer) {
+	io := NewIO(r, w)
+	io.PutString((io.nextString()))
+}
+
+type IO struct {
 	Scanner *bufio.Scanner
+	Writer  basicIO.Writer
 }
 
-func NewInPut() *InPut {
-	s := bufio.NewScanner(os.Stdin)
+func NewIO(r basicIO.Reader, w basicIO.Writer) *IO {
+	s := bufio.NewScanner(r)
 	s.Split(bufio.ScanWords)
-	return &InPut{Scanner: s}
+	return &IO{
+		Scanner: s,
+		Writer:  w,
+	}
 }
 
-func (s *InPut) nextString() string {
-	s.Scanner.Scan()
-	return s.Scanner.Text()
+func (io *IO) nextString() string {
+	io.Scanner.Scan()
+	return io.Scanner.Text()
 }
 
-func (s *InPut) nextInt() (int, error) {
-	s.Scanner.Scan()
-	i, err := strconv.Atoi(s.Scanner.Text())
+func (io *IO) nextInt() (int, error) {
+	io.Scanner.Scan()
+	i, err := strconv.Atoi(io.Scanner.Text())
 	return i, err
+}
+
+func (io *IO) PutInt(v int) {
+	fmt.Fprintf(io.Writer, "%d\n", v)
+}
+
+func (io *IO) PutString(s string) {
+	fmt.Fprintf(io.Writer, "%s\n", s)
 }
