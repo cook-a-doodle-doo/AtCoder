2afab12055888de7641451795e37f022722230a3
cook-a-doodle-do <densukenul@gmail.com>
2019-03-24 16:06:44 +0900

    add A question of GrandContest 032

---
+++ GrandContest/032/A/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	basicIO "io"
+	"strings"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	inputString := "hoge"
+	expect := "hoge\n"
+
+	output := checker(inputString, protagonist)
+	if output != expect {
+		t.Errorf("expcet %v ,but %v", expect, output)
+	}
+}
+
+func checker(input string, f func(basicIO.Reader, basicIO.Writer)) string {
+	reader := strings.NewReader(input)
+	writer := bytes.NewBufferString("")
+	f(reader, writer)
+	return writer.String()
+}
