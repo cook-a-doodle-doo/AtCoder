4549ded83040c621edceb666424a17a919aa9ddf
cook-a-doodle-do <densukenul@gmail.com>
2019-04-07 12:54:35 +0900

    Beginner 123 is completed

--- Beginners/123/D/main.go
+++ Beginners/123/D/main.go
@@ -9,6 +9,13 @@ import (
 	"strconv"
 )
 
+type Element struct {
+	sum int
+	x   int
+	y   int
+	z   int
+}
+
 func main() {
 	protagonist(os.Stdin, os.Stdout)
 }
@@ -20,11 +27,14 @@ func protagonist(r basicIO.Reader, w basicIO.Writer) {
 	Z := io.nextInt()
 	K := io.nextInt()
 
-	fmt.Println(X, Y, Z, K)
 	var arrx, arry, arrz []int
 	for i := 0; i < X; i++ {
 		arrx = append(arrx, io.nextInt())
+	}
+	for i := 0; i < Y; i++ {
 		arry = append(arry, io.nextInt())
+	}
+	for i := 0; i < Z; i++ {
 		arrz = append(arrz, io.nextInt())
 	}
 
@@ -32,15 +42,47 @@ func protagonist(r basicIO.Reader, w basicIO.Writer) {
 	sort.Sort(sort.Reverse(sort.IntSlice(arry)))
 	sort.Sort(sort.Reverse(sort.IntSlice(arrz)))
 
-	fmt.Println(arrx, arry)
+	var pool []Element
+	pool = append(pool, Element{arrx[0] + arry[0] + arrz[0], 0, 0, 0})
 
-	var x, y, z int
 	for i := 0; i < K; i++ {
-		x++
-		y++
-		z++
-		io.PutInt(4)
+		maxNum := 0
+		for j, _ := range pool {
+			if pool[maxNum].sum < pool[j].sum {
+				maxNum = j
+			}
+		}
+		io.PutInt(pool[maxNum].sum)
+
+		x := pool[maxNum].x
+		y := pool[maxNum].y
+		z := pool[maxNum].z
+
+		hasx := false
+		hasy := false
+		hasz := false
+		for _, e := range pool {
+			if e.x == x+1 && e.y == y && e.z == z {
+				hasx = true
+			} else if e.x == x && e.y == y+1 && e.z == z {
+				hasy = true
+			} else if e.x == x && e.y == y && e.z == z+1 {
+				hasz = true
+			}
+		}
+		if !hasx && x < X-1 {
+			pool = append(pool, Element{arrx[x+1] + arry[y] + arrz[z], x + 1, y, z})
+		}
+		if !hasy && y < Y-1 {
+			pool = append(pool, Element{arrx[x] + arry[y+1] + arrz[z], x, y + 1, z})
+		}
+		if !hasz && z < Z-1 {
+			pool = append(pool, Element{arrx[x] + arry[y] + arrz[z+1], x, y, z + 1})
+		}
+
+		pool = unset(pool, maxNum)
 	}
+
 }
 
 type IO struct {
@@ -74,4 +116,11 @@ func (io *IO) PutInt(v int) {
 
 func (io *IO) PutString(s string) {
 	fmt.Fprintf(io.Writer, "%s\n", s)
+}
+
+func unset(s []Element, i int) []Element {
+	if i >= len(s) {
+		return s
+	}
+	return append(s[:i], s[i+1:]...)
 }
